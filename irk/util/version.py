8d14d27e0c01bf08dc62762a8547928548c4c726
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2018-06-21 21:44:59 -0400

    add better licensing info, allow for custom resolvers via python packages

--- irk/util/version.py
+++ irk/util/version.py
@@ -2,4 +2,6 @@ VERSION_NUM = 2
 VERSION_STRING = "0.2"
 
 COPYRIGHT = """irk  Copyright (C) 2018  Matthew Mirvish
-This program comes with ABSOLUTELY NO WARRANTY"""
+This program comes with ABSOLUTELY NO WARRANTY. This software is free
+software, and you are welcome to redistribute it under certain conditions; 
+for more information see the included LICENSE file."""
